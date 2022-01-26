4922c050cc1c4ccbe6016a1c1a5a323396219c4c
unknown <prodigy803>
2022-01-26 03:58:38 -0500

    First Commit from Free Code Camp

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+        title: "Smart Expense Analyser",
+        context: "Professional",
+        description: 
+        "Python based solution that extracts specific information from different format documents.",
+        image: "./project-1.gif",
+        link: "https://www.ey.com/en_us/assurance/private-client-audit-experience-service-offerings"
+    },
+    {
+        title: "Minutes of Meeting Summarizer",
+        context: "Professional",
+        description: 
+        "Python based solution that extracts specific information from the minutes of a meeting and summarizes the information.",
+        image: "./project-1.gif",
+        link: "https://www.ey.com/en_us/assurance/private-client-audit-experience-service-offerings"
+    },
+    {
+        title: "Lease Q-A Bot",
+        context: "Professional",
+        description: 
+        "Python based solution that extracts answers specific information based on a lease agreement document.",
+        image: "./project-1.gif",
+        link: "https://www.ey.com/en_us/assurance/private-client-audit-experience-service-offerings"
+    },
+    {
+        title: "Created a Reinforcement Learning Environment",
+        context: "Professional",
+        description: 
+        "Python based solution that simulates a Reinforcement Learning ",
+        image: "./project-1.gif",
+        link: "https://github.com/prodigy803/UB_CSE546_Project1"
+    }
+]
+
+export const testimonials = [
+    {
+        name: "Jaya Suriya",
+        company: "Ernst and Young",
+        Quote: 
+        "Pj is the best",
+        image: "./project-1.gif",
+    },
+    
+]
+
+export const skills = [
+    
+    "Python","SQL","DL"
+]
